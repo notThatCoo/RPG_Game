b1f0f9e99c9a1ff19b40622d56d42491ff982646
TregisD <leonardohongguzman@gmai.com>
2024-02-28 20:20:33 -0800

    fixed character class in branch

---
+++ header/character.hpp
@@ -0,0 +1,35 @@
+#include <string>
+
+using namespace std;
+
+#ifndef __CHARACTER_HPP__
+#define __CHARACTER_HPP__
+
+enum Race{HUMAN, ELF, DRAGANOID, DWARF};
+
+
+class Character {
+ protected:
+	Race HeroRace;
+	string HeroClass;
+  string name;
+	double health;
+  double speed;
+	double dexterity;
+  double strength;
+  double wisdom;
+  double intelligence;
+  bool status;
+
+ public:
+ 	Character(Race, const string &, double, double, double, double, double, double);
+ 	void setStats(string c);
+  Race getRace() const;
+  const string& getName() const;
+  double getHealth() const;
+ 	void damage(double d);
+ 	bool isAlive();
+ 
+ };
+
+#endif
--- src/character.cpp
+++ src/character.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-#include "../header/character.h"
-using namespace std;
-
-    
-Character::Character(Race Race, const string &name, double health, double speed, double dexterity, double strength, double wisdom, double intelligence)
-: type(type), name(name), health(health), dexterity(dexterity), strength(strength), wisdom(wisdom), intelligence(intelligence) {}
-#include <iostream>
 #include "../header/character.hpp"
 using namespace std;
 
-    
 Character::Character(Race HeroRace, const string &name, double health, double speed, double dexterity, double strength, double wisdom, double intelligence)
 : HeroRace(HeroRace), name(name), health(health), dexterity(dexterity), strength(strength), wisdom(wisdom), intelligence(intelligence) {}
 
@@ -36,76 +28,25 @@ void Character::setStats(string c){
         wisdom += 3;
         intelligence +=3;
     }
-
-}
-Race Character::getRace(){
-    return HeroRace; 
-
-}
-string Character::getStats(){
-    
-
-}
-void Character::damage(double d){
-
-}
-void Character::attack(Character&){
-
-}
-bool Character::isAlive(){
-    if(health < 1){
-        return false;
-    }
-    else{
-        return true;
-    }
-
-}
-void setStats(string c){
-    if (c == "Knight"|| c== "knight"){
-        health += 2;
-        dexterity -= 1;
-        strength += 2;
-        wisdom -= 4;
-        intelligence +=1;
-    }
-    if(c== "Archer"|| c== "archer"){
-        speed += 2;
-        dexterity += 1;
-        strength -= 2;
-        wisdom += 2;
-        intelligence -=3;
-    }
-    if(c== "Wizard" || c== "wizard"){
+    if(c== "Assassin" || c== "Assassin"){
         health -= 1;
-        speed -= 1;
-        dexterity -= 2;
-        strength -= 2;
-        wisdom += 3;
-        intelligence +=3;
+        speed += 1;
+        dexterity += 2;
+        strength += 1;
+        wisdom -= 3;
     }
 
 }
-HeroRace Character::getReace(){
-    return Race; 
+Race Character::getRace() const { return HeroRace; }
 
-}
-string Character::getStats(){
+const string &Character::getName() const { return name; }
+
+double Character::getHealth() const {return health; }
+
+void Character::damage(double d) { health -= d; }
+
+bool Character::isAlive(){
     
-
-}
-void damage(double d){
-
-}
-void attack(character& ){
-
-}
-bool isAlive(){
-    if(health < 1){
-        return false;
-    }
-    else{
-        return true;
-    }
-
+    if(health < 1){ return false; }
+    else { return true; }
 }
