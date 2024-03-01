2fcf83108dbc813ecfb6bd4739514c7e40f6ce93
TregisD <leonardohongguzman@gmai.com>
2024-02-29 22:58:26 -0800

    added an about the game section

--- main.cpp
+++ main.cpp
@@ -7,21 +7,42 @@ int main() {
     cout << "Fantasy RPG \n" << endl;
 
     int typeOfGame = 0;
+    int startGame = 0;
 
-    cout << "New game (1)" << endl << "or" << endl << "Saved game (2)" << endl;
+    cout << "New game (1)" << endl << "or" << endl << "About the game (2)" << endl;
 
     cin >> typeOfGame;
 
     if (cin.fail()) { throw runtime_error("Invalid input"); }
 
-    if (typeOfGame = 1) {
+    if (typeOfGame == 1) {
 
         //Create new game
 
     }
-    else if (typeOfGame = 2)    { 
+    else if (typeOfGame == 2)    { 
 
-        //Go into saved game
+        cout << "About the Game \n" << endl;
+        cout << "Pick a race and class" << endl;
+        cout << "They will dictate your stats" << endl;
+        cout << "Increasing specific stats makes you stronger" << endl;
+        cout << "In this game you will go on a journey" << endl;
+        cout << "You will fight many foes and eventually make it to the boss" << endl;
+        cout << "Along the way you will get stronger" << endl;
+        cout << "Hopefully you can slay your enemies \n" << endl;
+
+        cout << "Do you wish to play the game?" << endl;
+        cout << "Yes (1)" << endl << "or" << endl << "No (2)" << endl;
+
+        cin >> startGame;
+
+        if (cin.fail()) { throw runtime_error("Invalid input"); }
+
+        if (startGame == 1) { //Create a new game 
+        }
+        else if (startGame == 2) { return 1; }
+
+        else { throw runtime_error("Invalid input"); }
 
     }
     else { throw runtime_error("Invalid input"); }
