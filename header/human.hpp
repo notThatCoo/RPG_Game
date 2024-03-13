d95761df3956a56bc35582089f5d1b4f434b1d66
TregisD <leonardohongguzman@gmai.com>
2024-03-12 22:22:16 -0700

    added human class

---
+++ header/human.hpp
@@ -0,0 +1,14 @@
+#ifndef __HUMAN_HPP__
+#define __HUMAN_HPP__
+
+#include "../header/character.hpp"
+
+using namespace std;
+
+class Human : public Character {
+
+    Human(const string&, const string &, double , double, double, double, double, int);
+
+};
+
+#endif
---
+++ src/human.cpp
@@ -0,0 +1,6 @@
+#include "../header/human.hpp"
+
+using namespace std;
+
+Human::Human(const string&name, const string &myClass, double health, double dexterity, double strength, double wisdom, double intelligence, int numHealing)
+    : Character (HUMAN, name, myClass, health, dexterity, strength, wisdom, intelligence, numHealing){}
